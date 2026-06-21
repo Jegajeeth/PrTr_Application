100f70715d7535e54b4e88fc3bf108e129985323
jegajeeth <jegajeeth2019@gmail.com>
2026-06-21 23:54:01 +0530

    Added the function app backend api calls

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,36 @@
-import { defineConfig } from "vite";
-
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:7071",
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({mode}) => {
+
+  const rootDir = import.meta.dirname || ".";
+  const env = loadEnv(mode, rootDir, '');
+
+  console.log("env: ", env);
+  console.log("rootDir: ", rootDir);
+  console.log(env.VITE_BACKEND_API_URL);
+  console.log(env.VITE_FUNCTION_KEY);
+  console.log(mode);
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_API_URL,
+          changeOrigin: true,
+          secure: false,
+          configure: (proxy) => {
+            proxy.on('proxyReq', (proxyReq) => {
+              proxyReq.setHeader('x-functions-key', env.VITE_FUNCTION_KEY);
+            })
+          },
+          header: {
+            'x-functions-key': env.VITE_FUNCTION_KEY
+          }
+        },
       },
     },
-  },
+  }
 });
